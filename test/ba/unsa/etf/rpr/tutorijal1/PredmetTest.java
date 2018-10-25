82db5c55383802582f4d586bb9f191a6b83c2f91
nhadziosma1 <nhadziosma1@etf.unsa.ba>
2018-10-25 14:35:27 +0200

    Added initial classes for Unit Tests

---
+++ test/ba/unsa/etf/rpr/tutorijal1/PredmetTest.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr.tutorijal1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PredmetTest {
+
+}
---
+++ test/ba/unsa/etf/rpr/tutorijal1/StudentTest.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.tutorijal1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void getImeStudenta() {
+    }
+
+    @Test
+    void getPrezimeStudenta() {
+    }
+
+    @Test
+    void getBrojIndeksa() {
+    }
+
+
+    @Test
+    void setImeStudenta() {
+    }
+
+    @Test
+    void setPrezimeStudenta() {
+    }
+
+    @Test
+    void setBrojIndeksa() {
+    }
+}
